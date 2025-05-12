fd038fc5936629e8f7c9472bfea643cef4fb545b
Just Shaffer <just@shaffer.dog>
2025-05-12 10:22:01 -0700

    Update dpi_scale.gd

--- dpi_scale.gd
+++ dpi_scale.gd
@@ -2,7 +2,7 @@ extends Node
 
 func _ready() -> void:
 	var window := get_window()
-	window.content_scale_factor = maxf(DisplayServer.screen_get_dpi() / 128.0, 1.0)
+	window.content_scale_factor = DisplayServer.screen_get_scale()
 	if OS.get_name() != "Web":
 		window.position -= Vector2i(window.size * (window.content_scale_factor - 1.0)) / 2
 		window.size *= get_window().content_scale_factor
