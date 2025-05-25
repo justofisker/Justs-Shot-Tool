876d282c6e5421a81ea31b034d8fba8ad6af3c9e
Just Shaffer <just@shaffer.dog>
2025-05-25 03:09:55 -0700

    DPI Scale Settings Window

--- settings/settings_window.gd
+++ settings/settings_window.gd
@@ -3,6 +3,10 @@ extends Window
 @export var settings_edit_container : VBoxContainer
 
 func _ready() -> void:
+	if OS.get_name() != "Web":
+		var dpi_scale := Node.new()
+		dpi_scale.set_script(preload("res://dpi_scale.gd"))
+		add_child(dpi_scale)
 	close_requested.connect(_on_close_requested)
 	for child in settings_edit_container.get_children():
 		var property := child.name.to_snake_case()
