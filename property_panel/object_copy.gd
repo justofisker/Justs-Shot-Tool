2685c3ba75ddefbea43685eff132b5a38e5e2c07
Just Shaffer <just@shaffer.dog>
2025-06-04 15:40:55 -0700

    Kinda fix for web copy

--- property_panel/object_copy.gd
+++ property_panel/object_copy.gd
@@ -8,7 +8,4 @@ func _on_copy_button_pressed() -> void:
 	for idx in Bridge.selected_object.projectiles.size():
 		copy_string += Bridge.selected_object.projectiles[idx].to_xml(idx)
 		
-	if OS.get_name() == "Web":
-		JavaScriptBridge.eval("window.copied_text = '%s'" % copy_string.c_escape())
-	else:
-		DisplayServer.clipboard_set(copy_string)
+	DisplayServer.clipboard_set(copy_string)
