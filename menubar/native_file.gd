f929f293eb2dcf1ef0e1d8d526aaac5daee16113
Just Shaffer <just@shaffer.dog>
2025-06-05 18:48:07 -0700

    Use DisplayServer file dialog

--- menubar/native_file.gd
+++ menubar/native_file.gd
@@ -34,51 +34,34 @@ func _on_file_id_pressed(id: int) -> void:
 			if current_file == "":
 				save_as()
 			else:
-				_on_save_file_selected(current_file)
+				_on_save_file_selected(true, [current_file], 0)
 		FileMenubar.Action.OpenScene:
-			var dialog := FileDialog.new()
-			dialog.add_filter("*.xml", "XML File")
-			dialog.use_native_dialog = true
-			dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-			add_child(dialog)
-			dialog.show()
-			dialog.file_selected.connect(_on_load_file_selected)
-			dialog.file_selected.connect(func(_path: String): dialog.queue_free())
-			dialog.canceled.connect(dialog.queue_free)
+			DisplayServer.file_dialog_show("Load Scene", "", "", false, DisplayServer.FILE_DIALOG_MODE_OPEN_FILE, ["*.xml"], _on_load_file_selected)
 		FileMenubar.Action.Import:
-			var dialog = FileDialog.new()
-			dialog.add_filter("*.xml", "XML File")
-			dialog.use_native_dialog = true
-			dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-			add_child(dialog)
-			dialog.show()
-			dialog.file_selected.connect(_on_import_file_selected)
-			dialog.file_selected.connect(func(_path: String): dialog.queue_free())
+			DisplayServer.file_dialog_show("Import Objects", "", "", false, DisplayServer.FILE_DIALOG_MODE_OPEN_FILE, ["*.xml"], _on_import_file_selected)
 
 func save_as() -> void:
-	var dialog = FileDialog.new()
-	dialog.add_filter("*.xml", "XML File")
-	dialog.current_file = "scene.xml"
-	dialog.use_native_dialog = true
-	add_child(dialog)
-	dialog.show()
-	dialog.file_selected.connect(_on_save_file_selected)
-	dialog.file_selected.connect(func(_path: String): dialog.queue_free())
-	dialog.canceled.connect(dialog.queue_free)
+	DisplayServer.file_dialog_show("Save Scene", "", "scene.xml", false, DisplayServer.FILE_DIALOG_MODE_SAVE_FILE, ["*.xml"], _on_save_file_selected)
 
-func _on_save_file_selected(path: String) -> void:
-	var file = FileAccess.open(path, FileAccess.WRITE)
-	file.store_string(%SceneManager.get_scene_xml())
-	current_file = path
+func _on_save_file_selected(status: bool, selected_paths: PackedStringArray, _selected_filter_index: int) -> void:
+	if status:
+		var path = selected_paths[0]
+		var file = FileAccess.open(path, FileAccess.WRITE)
+		file.store_string(%SceneManager.get_scene_xml())
+		current_file = path
 
-func _on_load_file_selected(path: String) -> void:
-	var file := FileAccess.open(path, FileAccess.READ)
-	%SceneManager.load_scene_xml(file.get_buffer(file.get_length()))
-	current_file = path
+func _on_load_file_selected(status: bool, selected_paths: PackedStringArray, _selected_filter_index: int) -> void:
+	if status:
+		var path = selected_paths[0]
+		var file := FileAccess.open(path, FileAccess.READ)
+		%SceneManager.load_scene_xml(file.get_buffer(file.get_length()))
+		current_file = path
 
-func _on_import_file_selected(path: String) -> void:
-	var file := FileAccess.open(path, FileAccess.READ)
-	if !is_instance_valid(import_window):
-		import_window = IMPORT_WINDOW.instantiate()
-		get_parent().add_child(import_window)
-		import_window.open_file(file.get_buffer(file.get_length()))
+func _on_import_file_selected(status: bool, selected_paths: PackedStringArray, _selected_filter_index: int) -> void:
+	if status:
+		var path = selected_paths[0]
+		var file := FileAccess.open(path, FileAccess.READ)
+		if !is_instance_valid(import_window):
+			import_window = IMPORT_WINDOW.instantiate()
+			get_parent().add_child(import_window)
+			import_window.open_file(file.get_buffer(file.get_length()))
