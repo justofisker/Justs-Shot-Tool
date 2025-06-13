102ef9bed8e13a852cf9d6599c4c72ba3a4f8d8f
Just Shaffer <just@shaffer.dog>
2025-06-13 15:07:23 -0700

    Add build targets

--- upload.ps1
+++ upload.ps1
@@ -8,6 +8,9 @@ function New-TemporaryDirectory {
 
 $TARGET_WEB = "justofisker/rotmg-shot-tool:html5"
 $TARGET_WINDOWS = "justofisker/rotmg-shot-tool:windows"
+$TARGET_WINDOWS_ARM = "justofisker/rotmg-shot-tool:windows-arm"
+$TARGET_LINUX = "justofisker/rotmg-shot-tool:linux"
+$TARGET_LINUX_ARM = "justofisker/rotmg-shot-tool:linux-arm"
 
 # Web Build
 $WEB_BUILD_DIR = New-TemporaryDirectory
@@ -25,3 +28,36 @@ Get-ChildItem -Path "$($WINDOWS_BUILD_DIR.FullName)\scripts" -Recurse -Filter *.
 New-Item -ItemType Directory -Path "$($WINDOWS_BUILD_DIR.FullName)\scenes" | Out-Null
 butler push "$($WINDOWS_BUILD_DIR.FullName)" "$TARGET_WINDOWS"
 Remove-Item -Recurse -Force $WINDOWS_BUILD_DIR
+
+# Windows Arm Build
+$WINDOWS_ARM_BUILD_DIR = New-TemporaryDirectory
+& $env:GODOT_BIN --headless --export-release "Windows Arm Desktop" "$($WINDOWS_ARM_BUILD_DIR.FullName)\Just's Shot Tool.exe" | Out-Host
+Copy-Item -Recurse -Force assets "$($WINDOWS_ARM_BUILD_DIR.FullName)\assets"
+Get-ChildItem -Path "$($WINDOWS_ARM_BUILD_DIR.FullName)\assets" -Recurse -Filter *.import | Remove-Item -Force
+Copy-Item -Recurse -Force scripts "$($WINDOWS_ARM_BUILD_DIR.FullName)\scripts"
+Get-ChildItem -Path "$($WINDOWS_ARM_BUILD_DIR.FullName)\scripts" -Recurse -Filter *.uid | Remove-Item -Force
+New-Item -ItemType Directory -Path "$($WINDOWS_ARM_BUILD_DIR.FullName)\scenes" | Out-Null
+butler push "$($WINDOWS_ARM_BUILD_DIR.FullName)" "$TARGET_WINDOWS_ARM"
+Remove-Item -Recurse -Force $WINDOWS_ARM_BUILD_DIR
+
+# Linux Build
+$LINUX_BUILD_DIR = New-TemporaryDirectory
+& $env:GODOT_BIN --headless --export-release "Linux" "$($LINUX_BUILD_DIR.FullName)\Just's Shot Tool" | Out-Host
+Copy-Item -Recurse -Force assets "$($LINUX_BUILD_DIR.FullName)\assets"
+Get-ChildItem -Path "$($LINUX_BUILD_DIR.FullName)\assets" -Recurse -Filter *.import | Remove-Item -Force
+Copy-Item -Recurse -Force scripts "$($LINUX_BUILD_DIR.FullName)\scripts"
+Get-ChildItem -Path "$($LINUX_BUILD_DIR.FullName)\scripts" -Recurse -Filter *.uid | Remove-Item -Force
+New-Item -ItemType Directory -Path "$($LINUX_BUILD_DIR.FullName)\scenes" | Out-Null
+butler push "$($LINUX_BUILD_DIR.FullName)" "$TARGET_LINUX"
+Remove-Item -Recurse -Force $LINUX_BUILD_DIR
+
+# Linux Arm Build
+$LINUX_ARM_BUILD_DIR = New-TemporaryDirectory
+& $env:GODOT_BIN --headless --export-release "Linux Arm" "$($LINUX_ARM_BUILD_DIR.FullName)\Just's Shot Tool" | Out-Host
+Copy-Item -Recurse -Force assets "$($LINUX_ARM_BUILD_DIR.FullName)\assets"
+Get-ChildItem -Path "$($LINUX_ARM_BUILD_DIR.FullName)\assets" -Recurse -Filter *.import | Remove-Item -Force
+Copy-Item -Recurse -Force scripts "$($LINUX_ARM_BUILD_DIR.FullName)\scripts"
+Get-ChildItem -Path "$($LINUX_ARM_BUILD_DIR.FullName)\scripts" -Recurse -Filter *.uid | Remove-Item -Force
+New-Item -ItemType Directory -Path "$($LINUX_ARM_BUILD_DIR.FullName)\scenes" | Out-Null
+butler push "$($LINUX_ARM_BUILD_DIR.FullName)" "$TARGET_LINUX_ARM"
+Remove-Item -Recurse -Force $LINUX_ARM_BUILD_DIR
--- upload.sh
+++ upload.sh
@@ -3,6 +3,9 @@ set -e
 
 TARGET_WEB="justofisker/rotmg-shot-tool:html5"
 TARGET_WINDOWS="justofisker/rotmg-shot-tool:windows"
+TARGET_WINDOWS_ARM="justofisker/rotmg-shot-tool:windows-arm"
+TARGET_LINUX="justofisker/rotmg-shot-tool:linux"
+TARGET_LINUX_ARM="justofisker/rotmg-shot-tool:linux-arm"
 
 # Web Build
 WEB_BUILD_DIR="$(mktemp -d)"
@@ -20,3 +23,36 @@ find "$WINDOWS_BUILD_DIR/scripts" -type f -name "*.uid" -delete
 mkdir "$WINDOWS_BUILD_DIR/scenes"
 butler push "$WINDOWS_BUILD_DIR" "$TARGET_WINDOWS"
 rm -rf "$WINDOWS_BUILD_DIR"
+
+# Windows Arm Build
+WINDOWS_ARM_BUILD_DIR="$(mktemp -d)"
+$GODOT_BIN --headless --export-release "Windows Arm Desktop" "$WINDOWS_ARM_BUILD_DIR/Just's Shot Tool.exe"
+cp -r assets "$WINDOWS_ARM_BUILD_DIR/"
+find "$WINDOWS_ARM_BUILD_DIR/assets" -type f -name "*.import" -delete
+cp -r scripts "$WINDOWS_ARM_BUILD_DIR/"
+find "$WINDOWS_ARM_BUILD_DIR/scripts" -type f -name "*.uid" -delete
+mkdir "$WINDOWS_ARM_BUILD_DIR/scenes"
+butler push "$WINDOWS_ARM_BUILD_DIR" "$TARGET_WINDOWS_ARM"
+rm -rf "$WINDOWS_ARM_BUILD_DIR"
+
+# Linux Build
+LINUX_BUILD_DIR="$(mktemp -d)"
+$GODOT_BIN --headless --export-release "Linux" "$LINUX_BUILD_DIR/Just's Shot Tool"
+cp -r assets "$LINUX_BUILD_DIR/"
+find "$LINUX_BUILD_DIR/assets" -type f -name "*.import" -delete
+cp -r scripts "$LINUX_BUILD_DIR/"
+find "$LINUX_BUILD_DIR/scripts" -type f -name "*.uid" -delete
+mkdir "$LINUX_BUILD_DIR/scenes"
+butler push "$LINUX_BUILD_DIR" "$TARGET_LINUX"
+rm -rf "$LINUX_BUILD_DIR"
+
+# Linux Arm Build
+LINUX_ARM_BUILD_DIR="$(mktemp -d)"
+$GODOT_BIN --headless --export-release "Linux Arm" "$LINUX_ARM_BUILD_DIR/Just's Shot Tool"
+cp -r assets "$LINUX_ARM_BUILD_DIR/"
+find "$LINUX_ARM_BUILD_DIR/assets" -type f -name "*.import" -delete
+cp -r scripts "$LINUX_ARM_BUILD_DIR/"
+find "$LINUX_ARM_BUILD_DIR/scripts" -type f -name "*.uid" -delete
+mkdir "$LINUX_ARM_BUILD_DIR/scenes"
+butler push "$LINUX_ARM_BUILD_DIR" "$TARGET_LINUX_ARM"
+rm -rf "$LINUX_ARM_BUILD_DIR"
